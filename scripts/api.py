664c392dd9f2c1ea7736f387ff8332e8b061f8d4
Lone-Hassan <lone.hassan@gmail.com>
2025-10-11 16:28:41 +0500

    Flask API for model serving

---
+++ scripts/api.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+
+# Load the model pipline
+model = joblib.load("../models/final_churn_pipeline.pkl")
+feature_names = model.named_steps["onehotencoder"].get_feature_names_out()
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Churn Prediction API is running!"})
+
+
+@app.route('/predict_churn', methods=['POST'])
+def predict_churn_endpoint():
+    
+    try:
+        
+        # Get data from request
+        data = request.get_json()
+        # Convert to DataFrame
+        df_input = pd.DataFrame(data)
+        # Make prediction
+        
+        probability = model.predict_proba(df_input)[:, 1][0]
+        prediction = model.predict(df_input)[0]
+        # Determine risk level
+        if probability > 0.7:
+            risk_level = 'High'
+        elif probability > 0.4:
+            risk_level = 'Medium'
+        else:
+            risk_level = 'Low'
+        response = {
+            'churn_probability': float(probability),
+            'churn_prediction': int(prediction),
+            'risk_level': risk_level,
+            'status': 'success'
+            }
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({'error': str(e), 'status': 'error'}), 400
+    
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'healthy'})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
